cbca8097aed301217412ba0220de0bef59586a8d
Dz_2_android2 <ermek250190>
2023-04-05 16:06:57 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import javax.management.openmbean.CompositeDataSupport;
+import java.util.Comparator;
+import java.util.Spliterator;
+import java.util.TreeSet;
+import java.util.function.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        BiConsumer<Integer,Integer>consumer=(x,y)-> System.out.println(x+" "+y);
+//        consumer.accept(100,109);
+
+//        Supplier<Integer>supplier=()-> 12334;
+//        System.out.println(supplier.get());
+
+//        BiPredicate<String,Integer>predicate= (x,y)->x.length()==y;
+//
+//        System.out.println(predicate.test("Hello",5));
+//
+//        Function<Integer,String>function=(x)->x.toString();
+//        System.out.println(function.apply(1234));
+
+//        BiFunction<Integer,Integer,Double>function=(x,y) -> ( x+y ) / 2.0;
+//
+//        System.out.println(function.apply(1234,12));
+        ///////////////////////////////////////////////////////////
+
+
+
+
+
+
+    }
+}
---
+++ src/com/company/Walkable.java
@@ -0,0 +1,8 @@
+package com.company;
+@FunctionalInterface
+public interface Walkable {
+
+    public void method1();
+}
+
+
